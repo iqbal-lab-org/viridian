08e19dd9811ba733daeb8a4d3bc3b46e677cb844
Jeff Knaggs <jknaggs@ebi.ac.uk>
2021-04-08 20:57:22 +0100

    black linter

--- viridian_workflow/one_sample_pipeline.py
+++ viridian_workflow/one_sample_pipeline.py
@@ -3,19 +3,33 @@ import os
 
 from viridian_workflow import minimap, qcovid
 
+
 def run_viridian(outdir, ref_genome, amplicon_bed, bam, bad_amplicons):
-    viridian_cmd = ['viridian', 'assemble', '--min_read_length', '50', '--bam', bam, '--amplicons_to_fail_file', bad_amplicons, ref_genome, amplicon_bed, outdir]
-    assembly = os.path.join(outdir, 'assembly.fa')
+    viridian_cmd = [
+        "viridian",
+        "assemble",
+        "--min_read_length",
+        "50",
+        "--bam",
+        bam,
+        "--amplicons_to_fail_file",
+        bad_amplicons,
+        ref_genome,
+        amplicon_bed,
+        outdir,
+    ]
+    assembly = os.path.join(outdir, "assembly.fa")
     subprocess.Popen(viridian_cmd).wait()
     assert os.path.isfile(assembly)
     return assembly
+
 
 def run_one_sample(outdir, ref_genome, amplicon_bed, fq1, fq2):
     bam = minimap.run(outdir, ref_genome, fq1, fq2)
     bad_amplicons = qcovid.bin_amplicons(outdir, ref_genome, amplicon_bed, bam)
 
-    viridian_out = os.path.join(outdir, 'viridian')
+    viridian_out = os.path.join(outdir, "viridian")
     assembly = run_viridian(viridian_out, ref_genome, amplicon_bed, bam, bad_amplicons)
 
     self_map = minimap.run(outdir, assembly, fq1, fq2)
-    #qcovid.self_qc(outdir, assembly, self_mapping)
+    # qcovid.self_qc(outdir, assembly, self_mapping)
