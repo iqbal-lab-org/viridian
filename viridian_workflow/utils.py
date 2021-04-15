24d38f0d7f789ee95824a88fd827f53953eaf02c
Jeff Knaggs <jknaggs@ebi.ac.uk>
2021-04-15 15:07:15 +0100

    log runtime of external processes

--- viridian_workflow/utils.py
+++ viridian_workflow/utils.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import subprocess
+import time
 
 
 class OutputFileError(Exception):
@@ -26,6 +27,7 @@ def run_process(cmd, ignore_error=False, stdout=None):
     stdout_fd = subprocess.PIPE
     if stdout:
         stdout_fd = open(stdout, "w")
+    start_time = time.time()
     result = subprocess.run(
         cmd,
         shell=True,
@@ -33,6 +35,8 @@ def run_process(cmd, ignore_error=False, stdout=None):
         stdout=stdout_fd,
         universal_newlines=True,
     )
+    time_elapsed = time.time() - start_time
+    logging.info(f"Process ({cmd}) completed in {time_elapsed} seconds.")
     if not stdout:
         logging.info(result.stdout)
     if not ignore_error and result.returncode != 0:
