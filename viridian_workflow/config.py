f620d8aac3551f3cb70a0bb837a002aede83df28
Jeff Knaggs <jknaggs@ebi.ac.uk>
2021-04-27 13:49:11 +0100

    actually committing the config module placeholder

---
+++ viridian_workflow/config.py
@@ -0,0 +1,17 @@
+"""global config module to govern run parameters""" 
+
+import configparser
+
+configuration = configparser.ConfigParser()
+#configuration.read('config.ini')
+
+class config:
+    def __init__(self):
+        pass
+
+varifier = config()
+qcovid = config()
+
+qcovid.min_template_match_75 = float(0.5)
+qcovid.min_coverage = 10
+qcovid.variant_freq = float(0.5)
