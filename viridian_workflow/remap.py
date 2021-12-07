2f6c317ddaf696904828c55b4a5e80ecd6c80457
jeff-k <jeff_k@fastmail.com>
2021-12-07 23:12:02 +0000

    accumulate per-position stats in Stats object

--- viridian_workflow/remap.py
+++ viridian_workflow/remap.py
@@ -16,6 +16,84 @@ amplicon_set = sys.argv[2]
 
 Stats = namedtuple("Stats", ["alt_in_primer", "ref", "alts", "total"])
 
+
+class Stats:
+    def __init__(self):
+        self.alt_in_primer = 0
+        self.alts = 0
+        self.amps = defaultdict(int)
+        self.amps_total = defaultdict(int)
+        self.total = 0
+
+    def add_alt(self, alt, amplicon):
+        self.alts += 1
+        self.amps[amplicon] += 1
+        self.amps_total[amplicon] += 1
+        self.total += 1
+
+    def add_ref(self, amplicon):
+        self.total += 1
+        self.amps_total[amplicon] += 1
+
+    def score(self):
+        """return whether a position should be masked
+        """
+        return True
+
+    def __str__(self):
+        f = []
+        if len(self.amps_total) > 1:
+            return "-".join([f"{k}:{v}" for k, v in self.amps_total.items()])
+        if self.alts / self.total > 0.2:
+            return f"{self.alts}/{self.total}"
+        return "-"
+
+
+def cigar_to_alts(ref, query, cigar):
+    """Interpret cigar string and query sequence in reference
+    coords
+    """
+    positions = []
+    q_pos = 0
+    for op, count in cigar:
+        if op == 0:
+            # match/mismatch
+            for i in range(q_pos, q_pos + count):
+                positions.append((q_pos + i, query[i]))
+            q_pos += count
+
+        elif op == 1:
+            # insertion
+            positions.append((q_pos, query[q_pos : q_pos + count]))
+            q_pos += count
+
+        elif op == 2:
+            # deletion
+            pass
+
+        elif op == 3:
+            # ref_skip
+            pass
+
+        elif op == 4:
+            # soft clip
+            q_pos += count
+            pass
+
+        elif op == 5:
+            # hard clip
+            pass
+
+        else:
+            print(f"invalid cigar op {op}")
+
+    return positions
+
+
+def read_error_profile(read):
+    pass
+
+
 if __name__ == "__main__":
     amplicon_tsvs = [
         "../data/midnight-1200.qcovid.tsv",
@@ -42,30 +120,34 @@ if __name__ == "__main__":
         a = ref.map(r.seq)
         tags = get_tags(None, r)[sn]
         if len(tags) < 1:
-            continue
-        amplicon = amplicons[tags[0]]
+            amplicon = "none"
+        else:
+            amplicon = amplicons[tags[0]].name
+
+        alignment = None
         for x in a:
-            if not x.is_primary:
-                continue
+            if x.is_primary:
+                alignment = x
 
-            if x.strand != 1:
-                continue
+        if not alignment:
+            continue
 
-            if x.q_en - x.q_st != x.r_en - x.r_st:
-                continue
+        #            if alignment.strand != 1:
 
-            for ref_base, q, p in zip(
-                ref_seq[x.r_st : x.r_en], r.seq, range(x.r_st, x.r_en)
-            ):
-                if p not in stats:
-                    stats[p] = Stats(0, ref_base, 0, 0)
-                stats[p]._replace(total=stats[p].total + 1)
-                if ref_base != q:
-                    if p < amplicon.start + 25 or p > amplicon.end - 25:
-                        stats[p]._replace(alt_in_primer=stats[p].alt_in_primer + 1)
-                    else:
-                        stats[p]._replace(alts=stats[p].alts + 1)
-    #            print("\t", x.r_st, x.r_en, tags, in_primer)
+        alts = cigar_to_alts(ref_seq[alignment.r_st : alignment.r_en], r.seq, r.cigar)
+        for read_pos, base in alts:
+            position = read_pos + alignment.r_st
 
-    for p in stats:
-        print(stats[p])
+            if position not in stats:
+                stats[position] = Stats()
+            if base != ref_seq[position]:
+                stats[position].add_alt(None, amplicon)
+            else:
+                stats[position].add_ref(amplicon)
+
+    for p in sorted(stats.keys()):
+        if stats[p].total < 5:
+            continue
+        if str(stats[p]) == "-":
+            continue
+        print(p, stats[p])
