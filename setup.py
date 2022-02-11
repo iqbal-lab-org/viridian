d851fb0b9e4ff9b29a060835ec52d77ede8ca8f2
Martin Hunt <martin.g.hunt@gmail.com>
2022-02-11 11:39:24 +0000

    Version bump 0.3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="viridian_workflow",
-    version="0.3.4",
+    version="0.3.5",
     description="FIXME",
     packages=find_packages(),
     package_data={"viridian_workflow": ["amplicon_scheme_data/*"]},
