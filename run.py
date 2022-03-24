1ffdedbba70ea14e81b7adce6ae494c5f0d6ade6
jeff-k <jeff_k@fastmail.com>
2022-03-24 16:51:36 +0000

    use pathlib and tempfile context manager

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import sys
+from pathlib import Path
+import tempfile
 
 from viridian_workflow import primers, readstore, minimap, utils
 
@@ -18,43 +20,48 @@ for name, tsv in [
 ]:
     amplicon_sets.append(primers.AmpliconSet.from_tsv(tsv, name=name))
 
-# generate name-sorted bam from fastqs
-minimap.run(
-    work_dir / "testbam.bam", sys.argv[1], sys.argv[1], fq2=sys.argv[2], sort=False
-)
+# input file args, working directory, and reference file path
+fq1, fq2 = sys.argv[1], sys.argv[2]
+ref = Path("../covid/MN908947.fasta")
+with tempfile.TemporaryDirectory() as work_dir:
+    work_dir = Path(work_dir)
+    print(f"using {work_dir} {work_dir.exists()}")
 
-# pre-process input bam
-bam = readstore.Bam(work_dir / "unsorted.bam")
+    # generate name-sorted bam from fastqs
+    unsorted_bam = minimap.run(work_dir / "testbam.bam", ref, fq1, fq2=fq2, sort=False)
 
-# detect amplicon set
-amplicon_set = bam.detect_amplicon_set(amplicon_sets)
+    # pre-process input bam
+    bam = readstore.Bam(unsorted_bam)
 
-stats = bam.stats
+    # detect amplicon set
+    amplicon_set = bam.detect_amplicon_set(amplicon_sets)
 
-# construct readstore
-rs = readstore.ReadStore(amplicon_set, bam)
+    stats = bam.stats
 
-# downsample to viridian assembly
-failures = rs.make_reads_dir_for_viridian(work_dir / "amplicons", 1000)
+    # construct readstore
+    rs = readstore.ReadStore(amplicon_set, bam)
 
-for failure in failures:
-    print(failure.name, file=failed_amplicon_amps_fd)
+    # downsample to viridian assembly
+    failures = rs.make_reads_dir_for_viridian(work_dir / "amplicons", 1000)
 
-# run viridian
-def run_viridian(self):
-    logging.info("Making initial unmasked consensus using Viridian")
-    viridian_cmd = " ".join(
-        [
-            "viridian",
-            "assemble",
-            "--bam",
-            self.sampled_bam,
-            "illumina",
-            "--amplicons_to_fail_file",
-            failed_amps_file,
-            ref,
-            amplicon_json,
-            work_dir / "viridian",
-        ]
-    )
-    utils.run_process(viridian_cmd)
+    for failure in failures:
+        print(failure.name, file=failed_amplicon_amps_fd)
+
+    # run viridian
+    def run_viridian(self):
+        logging.info("Making initial unmasked consensus using Viridian")
+        viridian_cmd = " ".join(
+            [
+                "viridian",
+                "assemble",
+                "--bam",
+                self.sampled_bam,
+                "illumina",
+                "--amplicons_to_fail_file",
+                failed_amps_file,
+                ref,
+                amplicon_json,
+                work_dir / "viridian",
+            ]
+        )
+        utils.run_process(viridian_cmd)
