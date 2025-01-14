9511fcc8f3a4b77458bdf50aa907ffac8729be95
Martin Hunt <martinghunt@users.noreply.github.com>
2025-01-14 15:15:46 +0000

    Version bump 1.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="viridian",
-    version="1.3.1",
+    version="1.4.0",
     description="Consensus builder from amplicon sequenced virus reads",
     packages=find_packages(),
     package_data={"viridian": ["amplicon_scheme_data/*"]},
